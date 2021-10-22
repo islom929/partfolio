e415d89a5411c1f975af1430cd613d09b4120d8c
islom929 <islomsultonov0@gmail.com>
2021-10-22 22:00:27 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+
+const body = document.getElementById("body");
+
+
+
+// console.log();
+var typed = new Typed("#example", {
+  strings: [
+  "Islom",
+  "a Web-Developer",
+  "a Freelancer"
+  ],
+  typeSpeed: 90,
+  backspaceSpeed: 50,
+  loop:true
+});
+
+window.addEventListener('scroll', function() {
+  document.querySelector('.hero__nav').classList.add("hero__nav--scrolled");
+  
+});
+
+
+function changeColorDark () {
+  body.classList.toggle("dark");
+  body.classList.remove("blue");
+  body.classList.remove("white");
+}
+
+function changeColorWhite () {
+  body.classList.remove("pink");
+  body.classList.remove("gray");
+  body.classList.remove("blue");
+  body.classList.remove("yellow");
+  body.classList.remove("green");
+  body.classList.add("white");
+}
+
+function changeColorBlue () {
+  body.classList.remove("white");
+  body.classList.remove("pink");
+  body.classList.remove("gray");
+  body.classList.remove("yellow");
+  body.classList.remove("green");
+  body.classList.add("blue");
+}
+
+function changeColorPink () {
+  body.classList.remove("white");
+  body.classList.remove("blue");
+  body.classList.remove("gray");
+  body.classList.remove("yellow");
+  body.classList.remove("green");
+  body.classList.add("pink");
+}
+
+function changeColorGray () {
+  body.classList.remove("white");
+  body.classList.remove("pink");
+  body.classList.remove("blue");
+  body.classList.remove("yellow");
+  body.classList.remove("green");
+  body.classList.add("gray");
+}
+
+function changeColorYellow () {
+  body.classList.remove("white");
+  body.classList.remove("pink");
+  body.classList.remove("blue");
+  body.classList.remove("gray");
+  body.classList.remove("green");
+  body.classList.add("yellow");
+}
+
+function changeColorGreen () {
+  body.classList.remove("gray");
+  body.classList.remove("yellow");
+  body.classList.remove("white");
+  body.classList.remove("pink");
+  body.classList.remove("blue");
+  body.classList.add("green");
+}
+
+function colorWhiteBlue () {
+  if (body.classList.contains("dark")) {
+    changeColorWhite();
+  } else {
+    changeColorBlue();
+  }
+}
