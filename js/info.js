7d6d2a3e05a9bdd5109910a706a360805fd6923e
islom929 <islomsultonov0@gmail.com>
2022-05-10 12:52:21 +0500

    portfolio is updated

---
+++ js/info.js
@@ -0,0 +1,74 @@
+const projectsDesktop = [
+  {
+    title : 'Ecommerce',
+    img: '/img/img-ecommerce.png',
+    gitLink: 'https://github.com/islom929/eCommerce.git',
+    link: 'https://ecommerce-islom.netlify.app'
+  },
+  {
+    title : 'PayApi',
+    img : '/img/img-payapi.png',
+    gitLink: 'https://github.com/islom929/payAPI.git',
+    link: 'https://payapi-islom.netlify.app'
+  },
+  {
+    title: 'MyTeam',
+    img: '/img/img-myteam-1500px.png',
+    gitLink: 'https://github.com/islom929/myteam.git',
+    link: 'https://myteam-islom.netlify.app'
+  },
+  {
+    title: 'VetClinic',
+    img: '/img/img-vetclinic.png',
+    gitLink: 'https://github.com/islom929/vet-clinic.git',
+    link: 'https://vet-clinic-islom.netlify.app'
+  },
+  {
+    title: 'Telegram',
+    img: '/img/img-telegram.png',
+    gitLink: 'https://github.com/islom929/telegram.git',
+    link: 'https://telegram-1700px-islom.netlify.app'
+  },
+  {
+    title: 'Bookmark',
+    img: '/img/img-bookmark.png',
+    gitLink: 'https://github.com/islom929/bookmark.git',
+    link: 'https://bookmark-1280px-islom.netlify.app'
+  },
+  {
+    title: 'SunnySide',
+    img: '/img/img-sunnyside.png',
+    gitLink: 'https://github.com/islom929/sunnyside.git',
+    link: 'https://sunnyside-islom.netlify.app'
+  },
+  {
+    title: 'test',
+    img: 'test',
+    gitLink: 'test',
+    link: 'test'
+  },
+  {
+    title: 'test',
+    img: 'test',
+    gitLink: 'test',
+    link: 'test'
+  },
+  {
+    title: 'test',
+    img: 'test',
+    gitLink: 'test',
+    link: 'test'
+  },
+  {
+    title: 'test',
+    img: 'test',
+    gitLink: 'test',
+    link: 'test'
+  },
+  {
+    title: 'test',
+    img: 'test',
+    gitLink: 'test',
+    link: 'test'
+  },
+]
