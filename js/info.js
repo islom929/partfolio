45e3e7697342fdb54c0ef8b5d626fc53da77e486
islom929 <islomsultonov0@gmail.com>
2022-05-12 14:55:20 +0500

    html elements are removed and optimized

--- js/info.js
+++ js/info.js
@@ -6,6 +6,30 @@ const projectsDesktop = [
     link: 'https://ecommerce-islom.netlify.app'
   },
   {
+    title: 'Vue Carts',
+    img: '/img/vue-prakrika.png',
+    gitLink: 'https://github.com/islom929/vue-project-items.git',
+    link: 'https://vue-project-islom.netlify.app'
+  },
+  {
+    title: 'Pressa',
+    img: '/img/pressa.png',
+    gitLink: 'https://github.com/Sarvarbek2003/pressa-frontend',
+    link: 'https://pressauz.herokuapp.com/'
+  },
+  {
+    title: 'MacBro',
+    link: 'https://macbroo-islom-1440px.netlify.app/',
+    gitLink: 'https://github.com/islom929/macBroo.git',
+    img: '/img/macbro-desktop.png'
+  },
+  {
+    title: 'Chevrolet.uz',
+    link: 'https://chevrolet-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/chevrolet2.git',
+    img: '/img/chevrolet.png'
+  },
+  {
     title : 'PayApi',
     img : '/img/img-payapi.png',
     gitLink: 'https://github.com/islom929/payAPI.git',
@@ -42,33 +66,142 @@ const projectsDesktop = [
     link: 'https://sunnyside-islom.netlify.app'
   },
   {
-    title: 'test',
-    img: 'test',
-    gitLink: 'test',
-    link: 'test'
+    title: 'Coffee roaster',
+    link: 'https://coffeeroaster-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/coffeeroaster.git',
+    img: '/img/img-coffeeroasters.png'
   },
   {
-    title: 'test',
-    img: 'test',
-    gitLink: 'test',
-    link: 'test'
+    title: 'Scoot',
+    link: 'https://scoot-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/scoot.git',
+    img: '/img/img-scoot.png'
   },
   {
-    title: 'test',
-    img: 'test',
-    gitLink: 'test',
-    link: 'test'
+    title: 'Easy bank',
+    link: 'https://easybank-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/easybank.git',
+    img: '/img/img-easybank.png'
   },
   {
-    title: 'test',
-    img: 'test',
-    gitLink: 'test',
-    link: 'test'
+    title: 'Rock Papper Scissors',
+    link: 'https://rock-paper-scissors-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/rock-paper-scissors.git',
+    img: '/img/img-rock-papper.png'
   },
   {
-    title: 'test',
-    img: 'test',
-    gitLink: 'test',
-    link: 'test'
+    title: 'Pixer',
+    link: 'https://26-dars-imtxon-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/26-dars-imtxon.git',
+    img: '/img/img-pixer.png'
   },
+  {
+    title: 'Minimalist',
+    link: 'https://minimalist-scss-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/minimalist-scss.git',
+    img: '/img/img-minimalist.png'
+  },
+  {
+    title: 'AppleWatch',
+    link: 'https://20-dars-imtxon-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/20-dars-imtxon.git',
+    img: '/img/img-applewatch.png'
+  }
+]
+
+const projectsTablet = [
+  {
+    title : 'Ecommerce',
+    img: '/img/img-ecommerce.png',
+    gitLink: 'https://github.com/islom929/eCommerce.git',
+    link: 'https://ecommerce-islom.netlify.app'
+  },
+  {
+    title: 'Pressa',
+    img: '/img/pressa.png',
+    gitLink: 'https://github.com/Sarvarbek2003/pressa-frontend',
+    link: 'https://pressauz.herokuapp.com/'
+  },
+  {
+    title: 'Chevrolet.uz',
+    link: 'https://chevrolet-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/chevrolet2.git',
+    img: '/img/chevrolet.png'
+  },
+  {
+    title : 'PayApi',
+    img : '/img/img-payapi.png',
+    gitLink: 'https://github.com/islom929/payAPI.git',
+    link: 'https://payapi-islom.netlify.app'
+  },
+  {
+    title: 'MyTeam',
+    img: '/img/img-myteam-1500px.png',
+    gitLink: 'https://github.com/islom929/myteam.git',
+    link: 'https://myteam-islom.netlify.app'
+  },
+  {
+    title: 'Bookmark',
+    img: '/img/img-bookmark.png',
+    gitLink: 'https://github.com/islom929/bookmark.git',
+    link: 'https://bookmark-1280px-islom.netlify.app'
+  },
+  {
+    title: 'SunnySide',
+    img: '/img/img-sunnyside.png',
+    gitLink: 'https://github.com/islom929/sunnyside.git',
+    link: 'https://sunnyside-islom.netlify.app'
+  },
+  {
+    title: 'Coffee roaster',
+    link: 'https://coffeeroaster-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/coffeeroaster.git',
+    img: '/img/img-coffeeroasters.png'
+  },
+  {
+    title: 'Rock Papper Scissors',
+    link: 'https://rock-paper-scissors-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/rock-paper-scissors.git',
+    img: '/img/img-rock-papper.png'
+  }
+]
+
+
+const projectsMobile = [
+  {
+    title : 'Ecommerce',
+    img: '/img/img-ecommerce.png',
+    gitLink: 'https://github.com/islom929/eCommerce.git',
+    link: 'https://ecommerce-islom.netlify.app'
+  },
+  {
+    title: 'Pressa',
+    img: '/img/pressa.png',
+    gitLink: 'https://github.com/Sarvarbek2003/pressa-frontend',
+    link: 'https://pressauz.herokuapp.com/'
+  },
+  {
+    title: 'Chevrolet.uz',
+    link: 'https://chevrolet-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/chevrolet2.git',
+    img: '/img/chevrolet.png'
+  },
+  {
+    title : 'PayApi',
+    img : '/img/img-payapi.png',
+    gitLink: 'https://github.com/islom929/payAPI.git',
+    link: 'https://payapi-islom.netlify.app'
+  },
+  {
+    title: 'SunnySide',
+    img: '/img/img-sunnyside.png',
+    gitLink: 'https://github.com/islom929/sunnyside.git',
+    link: 'https://sunnyside-islom.netlify.app'
+  },
+  {
+    title: 'Coffee roaster',
+    link: 'https://coffeeroaster-islom.netlify.app',
+    gitLink: 'https://github.com/islom929/coffeeroaster.git',
+    img: '/img/img-coffeeroasters.png'
+  }
 ]
--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 const body = document.getElementById("body");
 let navIcon = document.querySelector('.nav__icon');
-
-
-// navIcon.addEventListener('click', function() {
-//   navIcon.classList.toggle("open");
-// })
-
+const colorBtns = document.querySelectorAll('.hero-right__btn-color')
 
 function navMenu () {
   body.querySelector("header").classList.toggle("nav__menu")
@@ -18,62 +13,86 @@ function changeColorDark () {
   body.classList.remove("white");
 }
 
-function changeColorWhite () {
-  body.classList.remove("pink");
-  body.classList.remove("gray");
-  body.classList.remove("blue");
-  body.classList.remove("yellow");
-  body.classList.remove("green");
+const clearClassBody = () => {
+  colorBtns.forEach(btn => {
+    body.classList.remove(btn.dataset.color)
+  })
 }
 
-function changeColorBlue () {
-  body.classList.remove("white");
-  body.classList.remove("pink");
-  body.classList.remove("gray");
-  body.classList.remove("yellow");
-  body.classList.remove("green");
-}
+colorBtns.forEach(btn => {
+ btn.addEventListener('click', evt => {
+  clearClassBody()
 
-function changeColorPink () {
-  body.classList.remove("white");
-  body.classList.remove("blue");
-  body.classList.remove("gray");
-  body.classList.remove("yellow");
-  body.classList.remove("green");
-  body.classList.add("pink");
-}
+  body.classList.add(evt.target.dataset.color)
+})
+})
 
-function changeColorGray () {
-  body.classList.remove("white");
-  body.classList.remove("pink");
-  body.classList.remove("blue");
-  body.classList.remove("yellow");
-  body.classList.remove("green");
-  body.classList.add("gray");
-}
+document.querySelector('.desktop-btn').addEventListener('click', evt => {
 
-function changeColorYellow () {
-  body.classList.remove("white");
-  body.classList.remove("pink");
-  body.classList.remove("blue");
-  body.classList.remove("gray");
-  body.classList.remove("green");
-  body.classList.add("yellow");
-}
+document.querySelector('#desktopModal').innerHTML = `
+  <div class="modal-dialog mx-auto">
+  <div class="modal-content">
+    <div class="modal-body">
+      <div class="project-modal__box desktop-modal__box d-flex"></div>
+    </div>
+  </div>
+  </div>
+`
+projectsDesktop.forEach(el => {
+  const link = document.createElement('a')
+  link.classList.add('project-modal__link')
+  link.setAttribute('href', el.link)
+  link.setAttribute('target', '_blank')
+  link.innerHTML = `
+    <img class="project-modal__img" src="${el.img}" width={300} height={250} alt="img ${el.title}" />
+  `
+  document.querySelector('.desktop-modal__box').appendChild(link)
+  })
+})
 
-function changeColorGreen () {
-  body.classList.remove("gray");
-  body.classList.remove("yellow");
-  body.classList.remove("white");
-  body.classList.remove("pink");
-  body.classList.remove("blue");
-  body.classList.add("green");
-}
+document.querySelector('.tablet-btn').addEventListener('click', evt => {
 
-function colorWhiteBlue () {
-  if (body.classList.contains("dark")) {
-    changeColorWhite();
-  } else {
-    changeColorBlue();
-  }
-}
+document.querySelector('#tabletModal').innerHTML = `
+  <div class="modal-dialog mx-auto">
+  <div class="modal-content ">
+    <div class="modal-body">
+      <div class="project-modal__box tablet-modal__box d-flex"></div>
+    </div>
+  </div>
+  </div>
+`
+projectsTablet.forEach(el => {
+  const link = document.createElement('a')
+  link.classList.add('project-modal__link')
+  link.setAttribute('href', el.link)
+  link.setAttribute('target', '_blank')
+  link.innerHTML = `
+    <img class="project-modal__img" src="${el.img}" width={300} height={250} alt="img ${el.title}" />
+  `
+  document.querySelector('.tablet-modal__box').appendChild(link)
+  })
+})
+
+document.querySelector('.mobile-btn').addEventListener('click', evt => {
+
+document.querySelector('#mobileModal').innerHTML = `
+  <div class="modal-dialog mx-auto">
+  <div class="modal-content ">
+    <div class="modal-body">
+      <div class="project-modal__box mobile-modal__box d-flex"></div>
+    </div>
+  </div>
+  </div>
+`
+
+projectsMobile.forEach(el => {
+  const link = document.createElement('a')
+  link.classList.add('project-modal__link')
+  link.setAttribute('href', el.link)
+  link.setAttribute('target', '_blank')
+  link.innerHTML = `
+    <img class="project-modal__img" src="${el.img}" width={300} height={250} alt="img ${el.title}" />
+  `
+  document.querySelector('.mobile-modal__box').appendChild(link)
+  })
+})
